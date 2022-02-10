81648508bbdcb436a19f8ed639b4b32e2ab0fe31
ThirdRailgun <7845992+toss-a-coin-railgun@users.noreply.github.com>
2022-02-10 13:59:29 +1100

    added the vigenere cipher
    it currently does not work

--- cipher.py
+++ cipher.py
@@ -1,5 +1,6 @@
-def cipher_ceasar_encrypt(plaintext, key):
+def cipher_ceasar_encrypt(plaintext: str, key: int):
     ciphertext = ""
+
     for letter in plaintext:
         if ord(letter.lower()) not in range(97,123):
             ciphertext += letter
@@ -18,8 +19,40 @@ def cipher_ceasar_encrypt(plaintext, key):
     #return "".join([(chr(ord(letter) + (int(key) % 26))) for letter in str(plaintext)])
     #original attempt at list comprehension but it got too complex for its gracefulness.
 
+#current draft of the vigenere cipher.
+#ISSUE: The vigenere cipher ignores spaces but the keystream does not.
+def cipher_vigenere_encrypt(plaintext: str, key: str):
+    ciphertext = ""
+    keystream = []
+
+    for letter in plaintext:
+        if ord(letter.lower()) not in range(97,123):
+            ciphertext += letter
+            continue
+
+    #generate keystream that needs to be the same length as the plaintext from the key.
+    while (len(plaintext) > len(keystream)):
+        keystream.extend(list(key))
+    else:
+        if len(plaintext) < len(keystream):
+            keystream = keystream[:-(len(keystream)-len(plaintext))]
+
+    #convert the keystream to numbers, 1-26
+    keystream = [ord(k.lower()) - 96 for k in keystream]
+
+    #print(keystream)
+    #pass
+    #call the caesar cipher as the vigenere is just an extension of that.
+    for plaintext_letter, cipher_number in zip(plaintext, keystream):
+        #print(type(plaintext_letter), type(plaintext_letter))
+        ciphertext += cipher_ceasar_encrypt(plaintext_letter, cipher_number)
+
+    return ciphertext
+
 def main():
+    print(cipher_vigenere_encrypt("The quick brown fox jumps over the lazy dog.", "key"))
     pass
+
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -32,5 +32,9 @@ class test_cipher_caesar(unittest.TestCase):
     def test_real(self):
         assert cipher.cipher_ceasar_encrypt("The quick brown fox jumps over the lazy dog.", "23") == "Qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald."
 
+class test_cipher_vigenere(unittest.TestCase):
+    def test_encrypt1(self):
+        assert cipher.cipher_vigenere_encrypt("The quick brown fox jumps over the lazy dog.", "key") == "Dlc aygmo zbsux jmh nswtq yzcb xfo pyjc byk."
+
 if __name__ == '__main__':
     unittest.main()
