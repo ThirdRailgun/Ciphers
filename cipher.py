1d83dde0bf0f492d061d444c2b646bf6891accc8
ThirdRailgun <7845992+toss-a-coin-railgun@users.noreply.github.com>
2022-02-14 13:54:40 +1100

    updated method of determining punctuation
    there already exists a function for that

--- cipher.py
+++ cipher.py
@@ -1,20 +1,20 @@
 def cipher_ceasar_encrypt(plaintext: str, key: int):
-    ciphertext = ""
+    ciphertext = []
 
     for letter in plaintext:
-        if ord(letter.lower()) not in range(97,123):
-            ciphertext += letter
+        if letter.isalpha() == False:
+            ciphertext.append(letter)
             continue
 
         ciphered_letter_ascii = ord(letter.lower()) + (int(key) % 26)
         while ciphered_letter_ascii > 122: ciphered_letter_ascii -= 26
 
         if letter.isupper():
-            ciphertext += chr(ciphered_letter_ascii).upper()
+            ciphertext.append(chr(ciphered_letter_ascii).upper())
         else:
-            ciphertext += chr(ciphered_letter_ascii)
+            ciphertext.append(chr(ciphered_letter_ascii))
     
-    return ciphertext
+    return "".join(ciphertext)
             
     #return "".join([(chr(ord(letter) + (int(key) % 26))) for letter in str(plaintext)])
     #original attempt at list comprehension but it got too complex for its gracefulness.
